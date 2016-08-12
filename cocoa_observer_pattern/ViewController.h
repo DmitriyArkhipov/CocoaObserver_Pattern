d32a9f661711fe8c6fc050c4996392dbf9e3489d
Dmitriy Arkhipov <COODi93@mac-admin.local>
2016-08-12 13:23:43 +0800

    Initial Commit

---
+++ cocoa_observer_pattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoa_observer_pattern
+//
+//  Created by Dmitriy Arkhipov on 12.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
